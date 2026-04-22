5705a4d39e5738f75b7cbf7f54cb9c527f207f80
Naie Htoo Aung <k025g1013@.g.neec.ac.jp>
2026-04-22 15:38:18 +0900

    change

--- PG3_00_01/main.cpp
+++ PG3_00_01/main.cpp
@@ -1,56 +1,39 @@
-#include<stdio.h>
-#include <cstdlib>
+#include <stdio.h>
 #include <Windows.h>
+
 template <typename Type>
-	Type Min (Type a, Type b)
-	{
-		if (a > b)
-		{
-			return Type(b);
-		}
-		else {
-			return Type(a);
-		}
-	}
+Type Min(Type a, Type b)
+{
+    return (a > b) ? b : a;
+}
+
+template <>
+char Min<char>(char a, char b)
+{
+    printf("数字以外は代入できません\n");
+    return (a < b) ? a : b;
+}
+
 int main()
 {
-	SetConsoleOutputCP(65001);
-	int a = 0;
-	int b = 0;
-	int choice = 0;
-	float x = 0.0f;
-	float y = 0.0f;
-	printf_s("if it's int type 1 or float type 2\n");
-	scanf_s("%d",&choice);
-	if (choice == 1)
-	{
-		printf_s("input 2 int numbers\n");
-		if (scanf_s("%d %d", &a, &b) == 2)
-		{
-			printf_s("minimum(a,b)=%d\n", Min(a, b));
-		}
-		else
-		{
-			printf_s("数字以外代入できません");
-			return 1;
-		}
-	}
-	if (choice == 2)
-	{
-		printf_s("input 2 float numbers\n");
-		while (getchar() != '\n');
-		if (scanf_s("%f %f", &x, &y) == 2)
-		{
-			printf_s("minimum(x,y)=%.2f\n", Min(x, y));
-		}
-		else
-		{
-			printf_s("数字以外代入できません");
-			return 1;
-		}
-	}
-	
-	
-	
-	return 0;
+    SetConsoleOutputCP(65001);
+
+    // int
+    int a = 5, b = 3;
+    printf("int: %d\n", Min(a, b));
+
+    // float
+    float x = 2.5f, y = 1.2f;
+    printf("float: %.2f\n", Min(x, y));
+
+    // double
+    double d1 = 6.5, d2 = 4.5;
+    printf("double: %.2lf\n", Min(d1, d2));
+
+    // char
+    char c1 = 'A', c2 = 'B';
+	char result = Min(c1, c2);
+    printf("char: %c\n", result);
+
+    return 0;
 }
