154c5e681718a113305c562b5d4a9c04ccf9d151
Naie Htoo Aung <k025g1013@.g.neec.ac.jp>
2026-04-22 16:12:05 +0900

    change

--- PG3_00_01/main.cpp
+++ PG3_00_01/main.cpp
@@ -11,7 +11,7 @@ template <>
 char Min<char>(char a, char b)
 {
     printf("数字以外は代入できません\n");
-    return (a < b) ? a : b;
+    return 0; // 値は使わないのでダミー
 }
 
 int main()
@@ -30,10 +30,9 @@ int main()
     double d1 = 6.5, d2 = 4.5;
     printf("double: %.2lf\n", Min(d1, d2));
 
-    // char
+    // ⭐ char
     char c1 = 'A', c2 = 'B';
-	char result = Min(c1, c2);
-    printf("char: %c\n", result);
+    Min(c1, c2); 
 
     return 0;
 }
