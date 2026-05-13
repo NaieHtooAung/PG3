a004cf14bbddb6519d121206d7af85bf715cd5c7
Naie Htoo Aung <k025g1013@.g.neec.ac.jp>
2026-05-13 15:24:29 +0900

    change

--- PG3_00_01/main.cpp
+++ PG3_00_01/main.cpp
@@ -1,53 +1,56 @@
 #include <stdio.h>
 #include <Windows.h>
 
-
-// 再帰：n時間目の時給を求める
-int hourlyWage(int t) {
+// n時間目の時給
+int hourlyWage(int t)
+{
     if (t == 1) {
         return 100;
     }
+
     return hourlyWage(t - 1) * 2 - 50;
 }
 
-// 再帰：合計賃金を求める
-int totalSalary(int t) {
+// 合計賃金
+int totalSalary(int t)
+{
     if (t == 1) {
-        return 100;
+        return hourlyWage(1);
     }
+
     return totalSalary(t - 1) + hourlyWage(t);
 }
 
 int main()
 {
-    int choice = 0;
-    int time = 0;
-    int result = 0;
-
     SetConsoleOutputCP(65001);
 
-    char str[] = "働く時間を入力してください:";
-    printf("%s", str);
+    int time;
+    int choice;
+
+    printf("働く時間を入力してください: ");
     scanf_s("%d", &time);
-    char str2[] = "一般的な賃金体系は1番 / 再帰的な賃金は2番 を選んでください: ";
-    printf("%s", str2);
+
+    printf("一般的な賃金体系は1番 / 再帰的な賃金は2番 を選んでください: ");
     scanf_s("%d", &choice);
+
+    int result = 0;
 
     if (choice == 1)
     {
         result = time * 1072;
-       
-        printf("あなたの賃金は %d 円です\n", result);
     }
     else if (choice == 2)
     {
         result = totalSalary(time);
-        printf("あなたの賃金は %d 円です\n", result);
     }
     else
     {
         printf("無効な入力です\n");
+        return 0;
     }
+
+    printf("あなたの賃金は %d 円です\n", result);
 
     return 0;
 }
