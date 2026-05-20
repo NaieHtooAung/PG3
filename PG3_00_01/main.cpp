9466e762d2ec6636eae1b36ee111c80cdbbf606d
Naie Htoo Aung <k025g1013@.g.neec.ac.jp>
2026-05-20 16:39:14 +0900

    change

--- PG3_00_01/main.cpp
+++ PG3_00_01/main.cpp
@@ -1,44 +1,32 @@
 #include <stdio.h>
-#include <Windows.h>
 
-// 関数テンプレート
 template <typename T>
+
 T Min(T a, T b)
 {
-    return (a < b) ? a : b;
+	if (a < b)
+	{
+		return a;
+	}
 }
 
-// char型の特殊化
-template <>
+template<>
 char Min<char>(char a, char b)
 {
-    printf("数字以外は代入できません\n");
-    return 0;
+	printf("数字以外は代入出来ません");
+
+	return 0;
 }
 
-int main()
+int main(void)
 {
-    system("chcp 65001 > nul");
+	char chr1 = 'a';
+	char chr2 = 'b';
 
-    // int型
-    int i1 = 5;
-    int i2 = 3;
-    printf("int : %d\n", Min(i1, i2));
+	printf("%d\n", Min<int>(200, 500));
+	printf("%f\n", Min<float>(1.0f, 3.0f));
+	printf("%f\n", Min<double>(100.333, 80.333));
+	Min<char>(chr1, chr2);
 
-    // float型
-    float f1 = 2.5f;
-    float f2 = 1.2f;
-    printf("float : %.2f\n", Min(f1, f2));
-
-    // double型
-    double d1 = 6.5;
-    double d2 = 4.5;
-    printf("double : %.2lf\n", Min(d1, d2));
-
-    // char型
-    char c1 = 'A';
-    char c2 = 'B';
-    printf("char : %c\n", Min(c1, c2));
-
-    return 0;
+	return 0;
 }
