1063af55d27232c272bb24e61df17c7097a1196b
Naie Htoo Aung <k025g1013@.g.neec.ac.jp>
2026-05-20 16:47:27 +0900

    change tec

--- PG3_00_01/main.cpp
+++ PG3_00_01/main.cpp
@@ -1,32 +1,27 @@
-#include <stdio.h>
+#include <iostream>
+#include <string>
 
-template <typename T>
-
-T Min(T a, T b)
-{
-	if (a < b)
-	{
-		return a;
-	}
+template<typename T>
+const T& Min(const T& x, const T& y) {
+	return x < y ? x : y;
 }
 
 template<>
-char Min<char>(char a, char b)
-{
-	printf("数字以外は代入出来ません");
-
-	return 0;
+const char& Min<char>(const char& x, const char& y) {
+	static std::string str = "数字以外は代入できません";
+	return str.at(0);
 }
 
-int main(void)
-{
-	char chr1 = 'a';
-	char chr2 = 'b';
+int main() {
+	int32_t x_int = 114, y_int = 514;
+	float x_float = 514.0f, y_float = 1140.0f;
+	double x_double = 810.0, y_double = 1919.0;
+	char x_char = 'a', y_char = 'i';
 
-	printf("%d\n", Min<int>(200, 500));
-	printf("%f\n", Min<float>(1.0f, 3.0f));
-	printf("%f\n", Min<double>(100.333, 80.333));
-	Min<char>(chr1, chr2);
+	std::cout << Min(x_int, y_int) << std::endl;
+	std::cout << Min(x_float, y_float) << std::endl;
+	std::cout << Min(x_double, y_double) << std::endl;
+	std::wcout << &(Min(x_char, y_char)) << std::endl;
 
 	return 0;
 }
