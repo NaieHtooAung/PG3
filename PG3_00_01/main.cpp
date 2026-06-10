e1a367ea8581f296d8e4f955308c958147b78b31
Naie Htoo Aung <k025g1013@.g.neec.ac.jp>
2026-06-10 14:28:29 +0900

    fix

--- PG3_00_01/main.cpp
+++ PG3_00_01/main.cpp
@@ -1,46 +1,45 @@
 #include <stdio.h>
 #include <cstdlib>
 #include <Windows.h>
-#include <cmath>
 
-// 抽象クラス IShape
+const double PI = 3.14159265358979323846;
+
 class IShape {
 public:
-    virtual void Size() = 0;  // 純粋仮想関数
-    virtual void Draw() = 0;  // 純粋仮想関数
+    virtual void Size() = 0;
+    virtual void Draw() = 0;
     virtual ~IShape() {}
 };
 
-// 円クラス
 class Circle : public IShape {
 private:
-    double radius;  // 半径
+    double radius;
 public:
-    Circle(double r) : radius(r) {}
-
+    Circle(double r) {
+        radius = r;
+    }
     void Size() override {
-        double area = M_PI * radius * radius;
+        double area = PI * radius * radius;
         printf("円の面積: %.2f\n", area);
     }
-
     void Draw() override {
         printf("図形: 円、半径: %.2f\n", radius);
     }
 };
 
-// 矩形クラス
-class Rectangle : public IShape {
+class MyRectangle : public IShape {
 private:
-    double width;   // 幅
-    double height;  // 高さ
+    double width;
+    double height;
 public:
-    Rectangle(double w, double h) : width(w), height(h) {}
-
+    MyRectangle(double w, double h) {
+        width = w;
+        height = h;
+    }
     void Size() override {
         double area = width * height;
         printf("矩形の面積: %.2f\n", area);
     }
-
     void Draw() override {
         printf("図形: 矩形、幅: %.2f、高さ: %.2f\n", width, height);
     }
@@ -49,21 +48,17 @@ public:
 int main()
 {
     system("chcp 65001 > nul");
+    IShape* shape1 = new Circle(5.0);
+    IShape* shape2 = new MyRectangle(4.0f, 6.0f);
 
-    IShape* shapes[2];
-    shapes[0] = new Circle(5.0);
-    shapes[1] = new Rectangle(4.0, 6.0);
+    shape1->Draw();
+    shape1->Size();
+    printf("\n");
+    shape2->Draw();
+    shape2->Size();
 
-    for (int i = 0; i < 2; i++) {
-        shapes[i]->Draw();
-        shapes[i]->Size();
-        printf("\n");
-    }
-
-    // メモリ解放
-    for (int i = 0; i < 2; i++) {
-        delete shapes[i];
-    }
+    delete shape1;
+    delete shape2;
 
     return 0;
 }
